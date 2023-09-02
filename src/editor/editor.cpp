fe1653922e995728d2ebbaac9a92e206d9510c7a
Jannik Nickel <nickel.jannik1997@gmail.com>
2023-09-02 02:54:29 +0200

    Gradient editor UI

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -1,4 +1,5 @@
 #include "editor.h"
+#include "editorsettings.h"
 #include "guihelper.h"
 #include "particleobject.h"
 #include "spawnerobject.h"
@@ -11,14 +12,7 @@
 #include <cstdint>
 #include <format>
 
-static const Color previewValidColor = Color::From32(53, 132, 222, 128);
-static const Color previewInvalidColor = Color::From32(222, 53, 53, 128);
-static const Color sceneObjectColor = Color::From32(255, 255, 255, 200);
-static const Color selectedColor = Color::From32(222, 152, 53, 255);
-static const Color selectedHoverColor = Color::From32(222, 183, 111, 255);
-
-static const Color uiErrorColor = Color::From32(255, 53, 53, 200);
-static const Color uiSuccessColor = Color::From32(53, 222, 78, 200);
+using namespace EditorSettings;
 
 struct SimulationPopupData
 {
@@ -112,7 +106,7 @@ void Editor::Selection()
 	}
 
 	currentHovered = GetHoveredObject();
-	if(Input::KeyPressed(KeyCode::LMB) && !ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow | ImGuiHoveredFlags_AllowWhenBlockedByPopup))
+	if(Input::KeyPressed(KeyCode::LMB) && !ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow | ImGuiHoveredFlags_AllowWhenBlockedByPopup) && !ImGui::IsPopupOpen(nullptr, ImGuiPopupFlags_AnyPopup))
 	{
 		SelectObject(currentHovered);
 	}
---
+++ src/editor/editorsettings.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace EditorSettings
+{
+	static const Color previewValidColor = Color::From32(53, 132, 222, 128);
+	static const Color previewInvalidColor = Color::From32(222, 53, 53, 128);
+	static const Color sceneObjectColor = Color::From32(255, 255, 255, 200);
+	static const Color selectedColor = Color::From32(222, 152, 53, 255);
+	static const Color selectedHoverColor = Color::From32(222, 183, 111, 255);
+
+	static const Color uiErrorColor = Color::From32(255, 53, 53, 200);
+	static const Color uiSuccessColor = Color::From32(53, 222, 78, 200);
+}
